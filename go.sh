e23719a0df7c08214facdc86342cf055fc8c359e
Blair Nilsson <blair.nilsson@gmail.com>
2020-07-31 12:10:18 +1200

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,3 +1,5 @@
+set -e #always include this, it forces bash to exit when something actually fails, with an exit code, rather than 'this_is_fine.gif'
+
 echo "hey there, this should show up in the logs"
 echo "as should this"
 yo dog, lets fuck this shit up.
