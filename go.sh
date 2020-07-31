f4a0ab96ae4a7c0d289a59634e6331c1125a5c98
Blair Nilsson <blair.nilsson@gmail.com>
2020-07-31 12:31:57 +1200

    Update go.sh
    extra logging by default

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-set -e #always include this, it forces bash to exit when something actually fails, with an exit code, rather than 'this_is_fine.gif'
+set -ex #always include this, it forces bash to exit when something actually fails, with an exit code, rather than 'this_is_fine.gif'
 
 echo "hey there, this should show up in the logs"
 echo "as should this"
