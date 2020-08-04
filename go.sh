b3d25fb5ed277ad6e8bb5b5f67ee75a9bbe8d745
Sarah Wheaton <sarah.wheaton@gmail.com>
2020-08-04 14:29:08 +1200

    Update go.sh
    Now I are Bash

--- go.sh
+++ go.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -ex #always include this, it forces bash to exit when something actually fails, with an exit code, rather than 'this_is_fine.gif'
 
 echo "hey there, this should show up in the logs"
